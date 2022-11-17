a60c7e4aeb005b0bde66ffd399c135512451474f
haihuynh98er <haihuynh.dev@gmail.com>
2022-11-17 19:03:20 +0700

    remove category prefix

--- functions.php
+++ functions.php
@@ -1486,3 +1486,17 @@ function get_post_primary_category($post_id, $term='category', $return_all_categ
     return $return;
 }
 
+add_filter('get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif (is_tax()) { //for custom post types
+        $title = sprintf(__('%1$s'), single_term_title('', false));
+    } elseif (is_post_type_archive()) {
+        $title = post_type_archive_title('', false);
+    }
+    return $title;
+});
